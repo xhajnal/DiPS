9df22599541b4f0c7af137a2e365d2def662359c
Matej Hajnal <matej.hajnal@gmail.com>
2020-03-04 19:57:18 +0100

    models - bee(new) semisynch model added

---
+++ src/common/state_gen.py
@@ -0,0 +1,60 @@
+"""
+bee state space generator. Given population_size, write down all the states as a list s.t
+start with ones and then follows from 0 to population_size - 1 list of states
+"""
+
+
+def gen_state_encoding(population_size):
+    """ @author = Huy"""
+    state_encoding = [0, ]
+    for i in range(1, population_size):
+        state_encoding.extend([i, -i])
+    return state_encoding
+
+
+def map_state_code(state, state_encoding):
+    """ @author = Huy"""
+    return [state_encoding[item] for item in state]
+
+
+def backtrack(i, population_size, state, state_encoding) -> list:
+    """ @author = Huy"""
+    results = []
+    for v in range(len(state_encoding)):
+        state[i] = v
+        if i >= population_size - 1:
+            results.append(map_state_code(state, state_encoding))
+        else:
+            sub_results = backtrack(i + 1, population_size, state, state_encoding)
+            results.extend(sub_results)
+    return results
+
+
+def gen_full_statespace(population_size):
+    """ @author = Huy"""
+    start_state = [0] * population_size
+    results = backtrack(0, population_size, start_state, gen_state_encoding(population_size))
+    return results
+
+
+def gen_semisync_statespace(population_size):
+    """ @author = Huy"""
+    state_space = [tuple([population_size - 1] * population_size), [1] * population_size]
+    for i in range(population_size):
+        init_state = [0] * (population_size - i)
+        state_encoding = range(-i, 1)
+        sub_statespace = backtrack(0, population_size - i, init_state, state_encoding)
+        set_sub_statespace = set(tuple(sorted(l)) for l in sub_statespace)
+        for s in set_sub_statespace:
+            state = [1] * i + list(s)
+            state_space.append(state)
+    state_space = set(tuple(sorted(l, reverse=True)) for l in state_space)
+
+    return state_space
+
+
+if __name__ == "__main__":
+    ss = gen_semisync_statespace(6)
+    for s in ss:
+        print(s)
+    print(len(ss))
--- src/create_models_properties.py
+++ src/create_models_properties.py
@@ -2,6 +2,7 @@ import math
 from pathlib import Path
 import common.mathematics as mymath
 from common.config import load_config
+from common.state_gen import gen_semisync_statespace
 
 spam = load_config()
 model_path = spam["models"]
@@ -1375,16 +1376,197 @@ def create_bee_multiparam_synchronous_model(file, population_size):
                     file.write(f" + ")
             file.write(f";\n")
 
-    # All twos transitions
-    # file.write(f"       // all twos transition\n")
-    # file.write(f"       []   a0 = 2")
-    # for i in range(1, population_size):
-    #     file.write(f" & a{i} = 2 ")
-    # file.write(f"-> ")
-    # for i in range(population_size - 1):
-    #     file.write(f"(a{i}'= 0) & ")
-    # file.write(f"(a{(population_size-1)}'= 0)")
-    # file.write(f";\n")
+    file.write(f"endmodule \n")
+    file.write(f"\n")
+
+    # Rewards here
+    file.write('rewards "mean" \n')
+    for i in range(population_size + 1):
+        file.write(f"       a0 = {(1, 0)[i == 0]}")
+        for j in range(1, population_size):
+            # print(f"population_size",population_size,"i",i,"j",j)
+            file.write(f" & a{j} = " + str(1 if i > j else -i))
+        file.write(f":{i};\n")
+    file.write(f"endrewards \n")
+
+    file.write('rewards "mean_squared" \n')
+    for i in range(population_size + 1):
+        file.write(f"       a0 = {(1, 0)[i == 0]}")
+        for j in range(1, population_size):
+            # print(f"population_size",population_size,"i",i,"j",j)
+            file.write(f" & a{j} = " + str(1 if i > j else -i))
+        file.write(f":{i * i};\n")
+    file.write(f"endrewards \n")
+
+    file.close()
+
+
+def create_bee_multiparam_semisynchronous_model(file, population_size):
+    """ Creates semisynchronous model of *population_size* agents to a *file* with probabilities r_i in [0,1].
+
+    Args:
+        file (string): filename with extension
+        population_size (int):  agent quantity
+
+    Model meaning
+    ----------
+    Params:
+    population_size (int):  number of agents (agents quantity)
+    r_i (float): - probability of success of an agent when i amount of pheromone is present
+    """
+    filename = model_path / Path(file.split(".")[0] + ".pm")
+    file = open(filename, "w")
+    print(filename)
+
+    ## Computing how many zeros to put
+    decimals = math.ceil((math.log(population_size, 10)))
+
+    first_attempt = []
+    coefficient = ""
+
+    for i in range(population_size + 1):
+        for j in range(population_size - i):
+            coefficient = coefficient + f"*r_{0:0{decimals}d}"
+        for j in range(population_size - i, population_size):
+            coefficient = coefficient + f"*(1-r_{0:0{decimals}d})"
+        coefficient = str(mymath.nCr(population_size, i)) + coefficient
+        first_attempt.append(coefficient)
+        coefficient = ""
+
+    # Model starts here
+    file.write(f"dtmc \n \n")
+
+    for i in range(0, population_size):
+        file.write(f"const double r_{i:0{decimals}d};\n")
+    file.write(f"\n")
+
+    # Module here
+    file.write(f"module multi_param_bee_agents_{population_size}\n")
+    file.write(f"       // ai - state of agent i: 3:init 1:success -j: failure when j amount of pheromone present \n")
+    for i in range(population_size):
+        file.write(f"       a{i} : [-{population_size - 1}..3] init 3; \n")
+    file.write(f"       b : [0..1] init 0; \n")
+    file.write(f"\n")
+
+    # Transitions here
+    # Initial transition
+    file.write(f"       //  initial transitions\n")
+    file.write(f"       []   a0 = 3")
+    for i in range(1, population_size):
+        file.write(f" & a{i} = 3 ")
+    file.write(f" & b = 0 ")
+    file.write(f"-> ")
+    for i in range(population_size + 1):
+        file.write(first_attempt[i] + ": ")
+        for j in range(population_size):
+            file.write(f"(a{j}'=" + str(1 if population_size - i > j else -0) + ")")
+            if j < population_size - 1:
+                file.write(f" & ")
+        if i < population_size:
+            file.write(f" + ")
+    file.write(f";\n")
+    file.write(f"\n")
+
+    # Non-initial transition
+
+    # Some ones, some nonpositive final transitions
+    file.write(f"       // some ones, some nonpositive final transitions\n")
+    for i in range(population_size + 1):
+        file.write(f"       []   a0 = {(1, 0)[i == 0]}")
+        for j in range(1, population_size):
+            # print(f"population_size",population_size,"i",i,"j",j)
+            file.write(f" & a{j} = " + str(1 if i > j else -i))
+        file.write(f" & b = 0 ")
+        file.write(f" -> ")
+
+        for j in range(population_size - 1):
+            file.write(f"(a{j}'= " + str(1 if i > j else 0) + ") & ")
+        # print(f"population_size",population_size,"i",i)
+        file.write(f"(a{(population_size - 1)}'= " + str(1 if i == population_size else 0) + ")")
+        file.write(f" & (b'=1);\n")
+    file.write(f"\n")
+
+    # Some ones, some nonpositive transitions
+    file.write(f"       // some ones, some nonpositive transitions\n")
+
+    ## Get the full semisyn state space
+    states = gen_semisync_statespace(population_size)
+
+    for state in states:
+        ## skipping initial state
+        ## TODO correct this in huy script (should be if 3 in state)
+        if population_size - 1 in state:
+            continue
+        ## skipping all ones state
+        if all(map(lambda x: x == 1, state)):
+            continue
+        ## skipping all zeros state
+        if all(map(lambda x: x == 0, state)):
+            continue
+        ## skipping the rest of final states
+        ## if for all non ones the item is equal to ones, it means they cannot update any more
+        if all(map(lambda x: x == -state.count(1), list(filter(lambda x: x is not 1, state)))):
+            continue
+
+        # print("solving state", state)
+
+        ## Number of stinging bees
+        ones = state.count(1)
+
+        ## Distinct bee states which failed
+        distinct_fails = list(set(filter(lambda x: x is not 1, state)))
+        ## Filter those who can be updated
+        # print(distinct_fails)
+        distinct_fails = list(filter(lambda x: abs(x) < ones, distinct_fails))
+        # print(distinct_fails)
+
+        ## Double checking if at least one guy can be updated
+        if not distinct_fails:
+            continue
+
+        file.write(f"       []   a0 = {state[0]}")
+        for j in range(1, population_size):
+            # print(f"population_size",population_size,"i",i,"j",j)
+            file.write(f" & a{j} = {state[j]}")
+            # print(f" & a"+str(j)+" = "+str( 1 if i>=j else 0 ))
+        file.write(f" & b = 0 ")
+        file.write(f" -> ")
+
+        ## Probabilities and successor nodes
+        for index, fail_value in enumerate(distinct_fails):
+            fail_value = abs(fail_value)
+            for successes in [True, False]:
+                if successes:
+                    if len(distinct_fails) > 1:
+                        file.write(f"1/{len(distinct_fails)} * ")
+                    file.write(f"((r_{ones:0{decimals}d} - r_{fail_value:0{decimals}d})/(1 - r_{fail_value:0{decimals}d}))")
+                    new_state = list(state)
+                    new_state[new_state.index(-fail_value)] = 1
+                    new_state.sort(reverse=True)
+                    print("new state", new_state)
+                else:
+                    if len(distinct_fails) > 1:
+                        file.write(f"1/{len(distinct_fails)} * ")
+                    file.write(f"(1-(r_{ones:0{decimals}d} - r_{fail_value:0{decimals}d})/(1 - r_{fail_value:0{decimals}d}))")
+                    new_state = list(state)
+                    new_state[new_state.index(-fail_value)] = - ones
+                    new_state.sort(reverse=True)
+                    print("new state", new_state)
+                file.write(f": ")
+
+                for indexx, bee in enumerate(new_state):
+                    if indexx == len(new_state) - 1:
+                        file.write(f"(a{indexx}'={bee})")
+                    else:
+                        file.write(f"(a{indexx}'={bee})  & ")
+
+                if successes:
+                    file.write(f" + ")
+            ## If the last outgoing state
+            if index == len(distinct_fails) - 1:
+                file.write(f";\n")
+            else:
+                file.write(f"+")
 
     file.write(f"endmodule \n")
     file.write(f"\n")
@@ -1409,6 +1591,9 @@ def create_bee_multiparam_synchronous_model(file, population_size):
     file.write(f"endrewards \n")
 
     file.close()
+
+# population_size = 4
+# create_bee_multiparam_semisynchronous_model(f"bee_multiparam_semisynchronous_{population_size}", population_size)
 
 
 def create_properties(population_size):
