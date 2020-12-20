7d82f6e6604720ca9bbaf1a9fc61fb9744ef418e
Matej Hajnal <matej.hajnal@gmail.com>
2020-12-20 21:31:37 +0100

    examples - models, properties, and data added

---
+++ models/examples/bee/semisynchronous_3.pm
@@ -0,0 +1,53 @@
+dtmc 
+ 
+const double p;
+const double q;
+
+module two_param_agents_3
+       // ai - state of agent i:  -1:init, 0:total_failure, 1:success, 2:failure_after_first_attempt
+       // where success denotes decision to sting, failure the opposite
+       // b = 1: 'final'/leaf/BSCC state flag
+       a0 : [-1..2] init -1; 
+       a1 : [-1..2] init -1; 
+       a2 : [-1..2] init -1; 
+       b : [0..1] init 0; 
+
+       //  initial transition
+       []   a0 = -1 & a1 = -1  & a2 = -1 -> 1.0*p*p*p: (a0'=1) & (a1'=1) & (a2'=1) + 3.0*p*p*(1-p): (a0'=1) & (a1'=1) & (a2'=2) + 3.0*p*(1-p)*(1-p): (a0'=1) & (a1'=2) & (a2'=2) + 1.0*(1-p)*(1-p)*(1-p): (a0'=2) & (a1'=2) & (a2'=2);
+
+       // some ones, some zeros transitions
+       []   a0 = 0 & a1 = 0 & a2 = 0 -> (a0'= 0) & (a1'= 0) & (a2'= 0) & (b'=1);
+       []   a0 = 1 & a1 = 0 & a2 = 0 -> (a0'= 1) & (a1'= 0) & (a2'= 0) & (b'=1);
+       []   a0 = 1 & a1 = 1 & a2 = 0 -> (a0'= 1) & (a1'= 1) & (a2'= 0) & (b'=1);
+       []   a0 = 1 & a1 = 1 & a2 = 1 -> (a0'= 1) & (a1'= 1) & (a2'= 1) & (b'=1);
+
+       // some ones, some twos transitions
+       []   a0 = 1 & a1 = 2 & a2 = 2 -> q:(a0'= 1) & (a1'= 1) & (a2'= 2) + 1-q:(a0'= 1) & (a1'= 2) & (a2'= 0);
+       []   a0 = 1 & a1 = 1 & a2 = 2 -> q:(a0'= 1) & (a1'= 1) & (a2'= 1) + 1-q:(a0'= 1) & (a1'= 1) & (a2'= 0);
+
+       // some ones, some twos, some zeros transitions
+       []   a0 = 1 & a1 = 2 & a2 = 0 -> q: (a0' = 1) & (a1'= 1) & (a2'= 0) + 1-q: (a0' = 1) & (a1'= 0) & (a2'= 0);
+
+       // all twos transition
+       []   a0 = 2 & a1 = 2  & a2 = 2 -> (a0'= 0) & (a1'= 0) & (a2'= 0);
+endmodule 
+
+rewards "mean" 
+       a0 = 0 & a1 = 0 & a2 = 0:0;
+       a0 = 1 & a1 = 0 & a2 = 0:1;
+       a0 = 1 & a1 = 1 & a2 = 0:2;
+       a0 = 1 & a1 = 1 & a2 = 1:3;
+endrewards 
+rewards "mean_squared" 
+       a0 = 0 & a1 = 0 & a2 = 0:0;
+       a0 = 1 & a1 = 0 & a2 = 0:1;
+       a0 = 1 & a1 = 1 & a2 = 0:4;
+       a0 = 1 & a1 = 1 & a2 = 1:9;
+endrewards 
+rewards "mean_cubed" 
+       a0 = 0 & a1 = 0 & a2 = 0:0;
+       a0 = 1 & a1 = 0 & a2 = 0:1;
+       a0 = 1 & a1 = 1 & a2 = 0:8;
+       a0 = 1 & a1 = 1 & a2 = 1:27;
+endrewards 
+
--- src/performance_test/performance_test.py
+++ src/performance_test/performance_test.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from datetime import datetime
+from sympy import factor
 from time import time
 from termcolor import colored
 
@@ -25,6 +26,7 @@ model_path = spam["models"]
 property_path = spam["properties"]
 results_dir = spam["results"]
 prism_results = spam["prism_results"]
+storm_results = spam["storm_results"]
 tmp = spam["tmp"]
 del spam
 
@@ -32,17 +34,43 @@ sys.setrecursionlimit(4000000)
 timeout = 3600
 
 global debug
-globals()['debug'] = False
+globals()['debug'] = True
 
 
-def refine(population_size, n_samples, multiparam, parameters, parameter_domains, constraints, timeout):
+def load_functions(path, debug):
+    try:
+        functions = pickle_load(os.path.join(storm_results, path))
+        functions = list(map(factor, functions))
+        functions = list(map(str, functions))
+    except FileNotFoundError:
+        try:
+            functions = get_f(os.path.join(storm_results, path + ".txt"), "storm", factorize=False)
+            functions = list(map(factor, functions))
+            functions = list(map(str, functions))
+        except FileNotFoundError:
+            try:
+                functions = pickle_load(os.path.join(prism_results, path))
+                functions = list(map(factor, functions))
+                functions = list(map(str, functions))
+            except FileNotFoundError:
+                functions = get_f(os.path.join(prism_results, path + ".txt"), "prism", factorize=True)
+
+    print(colored(f"Loaded function file: {path}", "blue"))
+    if debug:
+        # print("functions", functions)
+        print("functions[0]", functions[0])
+    else:
+        print("functions[0]", functions[0])
+    return functions
+
+
+def refine(text, parameters, parameter_domains, constraints, timeout):
     max_depth = 15
     coverage = 0.95
     alg = 3
     epsilon = 0
     print(colored(
-        f"Refining, pop size {population_size}, dataset {data_index + 1}, multiparam {bool(multiparam)}, # of samples {n_samples}",
-        "yellow"))
+        f"Refining, {text}", "yellow"))
     print("Now computing, current time is: ", datetime.now())
     print("max_depth", max_depth, "coverage", coverage)
     space = RefinedSpace(parameter_domains, parameters)
@@ -56,8 +84,99 @@ def refine(population_size, n_samples, multiparam, parameters, parameter_domains
 
 
 if __name__ == '__main__':
+    # ## CROWDS
+    # model_file = os.path.join(model_path, f"crowds.pm")
+    # consts, parameters = parse_params_from_model(model_file, silent=True)
+    # property_file = os.path.join(property_path, f"crowds.pctl")
+    # if debug:
+    #     print("parameters", parameters)
+    #
+    # ## SETUP PARAMETER AND THEIR DOMAINS
+    # parameter_domains = []
+    # for item in parameters:
+    #     parameter_domains.append([0, 1])
+    # if debug:
+    #     print("parameter_domains", parameter_domains)
+    #
+    # ## LOAD FUNCTIONS
+    # functions = load_functions("crowds", debug)
+    #
+    # data_sets = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    # for data_set in data_sets:
+    #     data_set = [data_set]
+    #     if debug:
+    #         print("data_set", data_set)
+    #         print(type(data_set[0]))
+    #
+    #     ## COMPUTE INTERVALS
+    #     C = 0.95
+    #     n_samples = [100, 1500, 3500]
+    #
+    #     ## TODO more indices of n_samples
+    #     i = 0
+    #     intervals = []
+    #     for i in range(len(n_samples)):
+    #         intervals.append(create_intervals(float(C), int(n_samples[i]), data_set))
+    #         if debug:
+    #             print(f"Intervals, confidence level {C}, n_samples {n_samples[i]}: {intervals[i]}")
+    #
+    #     ## OPTIMIZE PARAMS
+    #     start_time = time()
+    #     result_1 = optimize(functions, parameters, parameter_domains, data_set)
+    #     print(colored(
+    #         f"Optimisation, data {data_set}, took {round(time() - start_time, 4)} seconds", "yellow"))
+    #     if debug:
+    #         print(result_1)
+    #
+    #     constraints = []
+    #     for i in range(len(n_samples)):
+    #         constraints.append(ineq_to_constraints(functions, intervals[i], decoupled=True))
+    #         if debug:
+    #             print(f"constraints with {n_samples[i]} samples :{constraints[i]}")
+    #
+    #     ## SAMPLE SPACE
+    #     grid_size = 25
+    #     for i in range(len(n_samples)):
+    #         space = RefinedSpace(parameter_domains, parameters)
+    #         start_time = time()
+    #         sampling = space.grid_sample(constraints[i], grid_size, silent=True, debug=debug)
+    #         print(colored(
+    #             f"Sampling, dataset {data_set}, # of samples {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
+    #         if debug:
+    #             print(sampling)
+    #
+    #     ## REFINE SPACE
+    #     for i in range(len(n_samples)):
+    #         refine(f"dataset {data_set}, # of samples {n_samples[i]}", parameters, parameter_domains, constraints, timeout)
+    #
+    #     ## METROPOLIS-HASTINGS
+    #     space = RefinedSpace(parameter_domains, parameters)
+    #     iterations = 500000
+    #     ## TODO more indices of n_samples
+    #     for i in range(len(n_samples)):
+    #         start_time = time()
+    #         mh_results = initialise_sampling(space, data_set, functions, n_samples[i], iterations, 0, where=True, metadata=False, timeout=timeout)
+    #         assert isinstance(mh_results, HastingsResults)
+    #         print(colored(
+    #             f"this was MH, dataset {data_set}, # of samples, {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
+    #         if debug:
+    #             print("# of accepted points", len(mh_results.accepted))
+    #             if mh_results.last_iter > 0:
+    #                 print("current iteration", mh_results.last_iter)
+    #                 iter_time = (time() - start_time) * (iterations / mh_results.last_iter)
+    #                 print("time it would take to finish", iter_time)
+    #             print()
+
+        # ## LIFTING WITH STORM HERE
+        # for i in range(len(n_samples)):
+        #     general_create_data_informed_properties(property_file, intervals[i], silent=False)
+        #     call_storm(model_file=model_file, params=parameters, param_intervals=parameter_domains,
+        #                property_file=property_file, storm_output_file=f"tmp_storm_pop_size_{population_size}_data_{data_index}_nsamples_{n_samples[i]}.txt",
+        #                time=True, silent=False)
+
+    ## semisynchronous models
     for multiparam in [False, True]:                                                ## [False, True]
-        for population_size in [2, 3, 4, 5, 10, 15]:                                          ## [2, 3, 4, 5, 10, 15]
+        for population_size in [15]:                                          ## [2, 3, 4, 5, 10, 15]
             for data_index, data_dir_subfolder in enumerate(["data", "data_1"]):    ## ["data", "data_1"]
                 if multiparam:
                     params = "multiparam"
@@ -84,15 +203,7 @@ if __name__ == '__main__':
                     print("parameter_domains", parameter_domains)
 
                 ## LOAD FUNCTIONS
-                try:
-                    functions = pickle_load(os.path.join(prism_results, f"{params}/semisynchronous/{prefix}semisynchronous_{population_size}.p"))
-                except FileNotFoundError:
-                    functions = get_f(os.path.join(prism_results, f"{params}/semisynchronous/{prefix}semisynchronous_{population_size}.txt"), "prism", factorize=True)
-                print(colored(f"Loaded function file: {params}/semisynchronous/{prefix}semisynchronous_{population_size}", "blue"))
-                if debug:
-                    print("functions", functions)
-                else:
-                    print("functions[0]", functions[0])
+                functions = load_functions(f"{params}/semisynchronous/{prefix}semisynchronous_{population_size}", debug)
 
                 ## LOAD DATA
                 data_set = load_data(os.path.join(data_dir, f"{data_dir_subfolder}/{params}/data_n={population_size}.csv"), debug=debug)
@@ -104,7 +215,7 @@ if __name__ == '__main__':
 
                 ## COMPUTE INTERVALS
                 C = 0.95
-                n_samples = [100, 1500, 3500]  ## [100, 1500, 3500]
+                n_samples = [3500, 1500]  ## [100, 1500, 3500]
 
                 ## TODO more indices of n_samples
                 i = 0
@@ -127,59 +238,41 @@ if __name__ == '__main__':
                     if debug:
                         print(f"constraints with {n_samples[i]} samples :{constraints[i]}")
 
-                ## SAMPLE SPACE
-                grid_size = 25
-                for i in range(len(n_samples)):
-                    space = RefinedSpace(parameter_domains, parameters)
-                    start_time = time()
-                    sampling = space.grid_sample(constraints[i], grid_size, silent=True, debug=debug)
-                    print(colored(f"Sampling, pop size {population_size}, dataset {data_index+1}, multiparam {bool(multiparam)}, # of samples {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
-                    if debug:
-                        print(sampling)
+                # ## SAMPLE SPACE
+                # grid_size = 25
+                # for i in range(len(n_samples)):
+                #     space = RefinedSpace(parameter_domains, parameters)
+                #     start_time = time()
+                #     sampling = space.grid_sample(constraints[i], grid_size, silent=True, debug=debug)
+                #     print(colored(f"Sampling, pop size {population_size}, dataset {data_index+1}, multiparam {bool(multiparam)}, # of samples {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
+                #     if debug:
+                #         print(sampling)
 
                 ## REFINE SPACE
                 for i in range(len(n_samples)):
-                    refine(population_size, n_samples[i], multiparam, parameters, parameter_domains, constraints, timeout)
+                    text = f"pop size {population_size}, dataset {data_index + 1}, multiparam {bool(multiparam)}, # of samples {n_samples}"
+                    refine(text, parameters, parameter_domains, constraints, timeout)
 
-                jobs = []
-                for i in range(len(n_samples)):
-                    timeout = 5
-                    refine(population_size, n_samples, multiparam, parameters, parameter_domains, constraints, timeout)
-                    p = multiprocessing.Process(target=refine, args=(population_size, n_samples, multiparam, parameters, parameter_domains, constraints, timeout))
-                    jobs.append(p)
-                    p.start()
+                # ## METROPOLIS-HASTINGS
+                # space = RefinedSpace(parameter_domains, parameters)
+                # iterations = 500000
+                # ## TODO more indices of n_samples
+                # for i in range(len(n_samples)):
+                #     start_time = time()
+                #     mh_results = initialise_sampling(space, data_set, functions, n_samples[i], iterations, 0, where=True, metadata=False, timeout=timeout)
+                #     assert isinstance(mh_results, HastingsResults)
+                #     print(colored(f"this was MH, pop size {population_size}, dataset {data_index+1}, multiparam {bool(multiparam)}, # of samples, {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
+                #     if debug:
+                #         print("# of accepted points", len(mh_results.accepted))
+                #         if mh_results.last_iter > 0:
+                #             print("current iteration", mh_results.last_iter)
+                #             iter_time = (time() - start_time) * (iterations / mh_results.last_iter)
+                #             print("time it would take to finish", iter_time)
+                #         print()
 
-                # for p in jobs:
-                    p.join(3600)
-
-                # for p in jobs:
-                    if p.is_alive():
-                        print("running... let's kill it...")
-
-                        # Terminate
-                        p.terminate()
-                        p.join()
-
-                ## METROPOLIS-HASTINGS
-                space = RefinedSpace(parameter_domains, parameters)
-                iterations = 500000
-                ## TODO more indices of n_samples
-                for i in range(len(n_samples)):
-                    start_time = time()
-                    mh_results = initialise_sampling(parameters, parameter_domains, data_set, functions, n_samples[i], iterations, 0, where=True, metadata=False, timeout=timeout)
-                    assert isinstance(mh_results, HastingsResults)
-                    print(colored(f"this was MH, pop size {population_size}, dataset {data_index+1}, multiparam {bool(multiparam)}, # of samples, {n_samples[i]} took {round(time() - start_time, 4)} seconds", "yellow"))
-                    if debug:
-                        print("# of accepted points", len(mh_results.accepted))
-                        if mh_results.last_iter > 0:
-                            print("current iteration", mh_results.last_iter)
-                            iter_time = (time() - start_time) * (iterations / mh_results.last_iter)
-                            print("time it would take to finish", iter_time)
-                        print()
-
-                ## LIFTING WITH STORM HERE
-                for i in range(len(n_samples)):
-                    general_create_data_informed_properties(property_file, intervals[i], silent=False)
-                    call_storm(model_file=model_file, params=parameters, param_intervals=parameter_domains,
-                               property_file=property_file, storm_output_file=f"tmp_storm_pop_size_{population_size}_data_{data_index}_nsamples_{n_samples[i]}.txt",
-                               time=True, silent=False)
+                # ## LIFTING WITH STORM HERE
+                # for i in range(len(n_samples)):
+                #     general_create_data_informed_properties(property_file, intervals[i], silent=False)
+                #     call_storm(model_file=model_file, params=parameters, param_intervals=parameter_domains,
+                #                property_file=property_file, storm_output_file=f"tmp_storm_pop_size_{population_size}_data_{data_index}_nsamples_{n_samples[i]}.txt",
+                #                time=True, silent=False)
